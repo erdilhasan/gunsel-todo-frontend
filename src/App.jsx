192664642e7814a3625c1ac36889a6195c918269
erdilhasan <hasanerdil1260@gmail.com>
2024-06-26 09:04:43 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import LoginTile from "./components/loginComponent.jsx";
+import TaskTile from "./components/task_tile.jsx";
+
+function App() {
+  return (
+    <>
+      <LoginTile></LoginTile>
+      <TaskTile></TaskTile>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/loginComponent.jsx
@@ -0,0 +1,53 @@
+import { useForm } from "react-hook-form";
+
+export default function LoginTile() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-sm">
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+            E-mail
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input
+            {...register("email")}
+            name="email"
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500"
+            type="text"
+            //  value="name"
+          />
+        </div>
+      </div>
+      <div className="md:flex md:items-center mb-6">
+        <div className="md:w-1/3">
+          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+            Password
+          </label>
+        </div>
+        <div className="md:w-2/3">
+          <input
+            {...register("password")}
+            name="password"
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500"
+            type="password"
+          />
+        </div>
+      </div>
+
+      <div className="md:flex md:items-center">
+        <div className="md:w-1/3"></div>
+        <div className="md:w-2/3">
+          <button className="shadow bg-indigo-500 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+            Login
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
